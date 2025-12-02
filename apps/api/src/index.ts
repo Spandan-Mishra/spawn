0510d8055dfa49a4fa190589ca9a2e5c088b3a0a
Spandan-Mishra <spandanmishra69@gmail.com>
2025-12-03 03:19:31 +0530

    project creation endpoint added

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,33 @@
-const express = require('express');
+import express from "express";
+import { prisma } from "@repo/db";
+import { BASE_TEMPLATE } from "./base-template";
 
 const app = express();
+app.use(express.json());
+
+app.post("/project", async (req, res) => {
+    const { prompt, userId } = req.body;
+    
+    await prisma.$transaction(async (tx: any) => {
+        const project = await tx.project.create({
+            data: {
+                userId
+            }
+        })
+
+        await Promise.all(Object.entries(BASE_TEMPLATE).map(async ([key, value]) => {
+            await tx.file.create({
+                data: {
+                    path: key,
+                    content: value,
+                    projectId:project.id
+                }
+            })
+        }))
+
+        res.json({ projectId: project.id });
+    })
+})
 
 app.listen(3001, () => {
     console.log("Spawn Backend started on port 3001");
